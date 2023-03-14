ba35c181be2eb32dd6ab63e358cd72f1c7046d92
Helliun <1617752537@qq.com>
2023-03-14 15:06:26 +0800

    second commit

---
+++ test/P2/rules/RulesOf6005Test.java
@@ -0,0 +1,18 @@
+package rules;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RulesOf6005Test {
+
+    @Test
+    public void mayUseCodeInAssignment() {
+    }
+
+    @Test
+    public void main() {
+    }
+
+
+}
